49bde0b967ad8866f948e94170603843bedaea0e
Calvin Ruocco <calvin.ruocco@mailbox.org>
2020-06-07 00:54:58 +0200

    Improve build.sh and deploy.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,72 @@
 #!/bin/bash
-build_dir="$1"
+outdir="$1"
+if [ -z "$outdir" ]; then
+    echo "Usage: $0 <output_dir>"
+    exit 1
+fi
+
 timestamp="$(date --utc +"%Y-%m-%d %H:%M UTC")"
 
-# Create build directory if not present
-mkdir -p "$build_dir"
+_headline () { echo "$(tput setaf 4)$1$(tput sgr0)"; }
+_info     () { echo "$(tput setaf 5)> $1$(tput sgr0)"; }
+_warning  () { echo "$(tput setaf 3)> $1$(tput sgr0)"; }
+_error    () { echo "$(tput setaf 1)Error: $1$(tput sgr0)"; }
+
+# ---
+_headline "Checking working directory status"
+
+status="$(git status --short --ignored)"
+if [ -z "$status" ]; then
+    _info "Working directory clean"
+else
+    _warning "Uncommited changes exist!"
+    echo "$status"
+fi
+
+# ---
+_headline "Preparing output directory"
+
+exitcode=0
+if [ -d "$outdir" ] && [ -e "${outdir}/.git" ]; then
+    _info "Clearing worktree contents of output directory"
+    cd "$outdir"
+    rm -f $(git ls-files --cached --other -- .)
+    exitcode=$?
+    cd ..
+elif [ -d "$outdir" ]; then
+    _info "Removing output directory"
+    rm -rf "$outdir"
+    exitcode=$?
+else
+    _info "Creating output directory"
+    mkdir "$outdir"
+    exitcode=$?
+fi
+
+if [ "$exitcode" -ne 0 ]; then
+    _error "Operation failed"
+    exit 1
+fi
+
+# ---
+_headline "Building site"
 
 # Build site
-echo "<!DOCTYPE html><html><head><title>${domain}</title></head><body>OK</body></html>" > "${build_dir}/index.html"
-#hugo --source site --destination "$build_dir"
+echo "<!DOCTYPE html><html><head><title>${domain}</title></head><body>OK</body></html>" > "${outdir}/index.html"
+#hugo --source site --destination "$outdir"
 
 # Create CNAME
 cname="ruoc.co"
-echo "$cname" > "${build_dir}/CNAME"
+echo "$cname" > "${outdir}/CNAME"
 
 # Create README
 rev="$(git rev-parse --short HEAD)"
-readme="Built on ${timestamp} based on revision ${rev}."
-mkdir -p "${build_dir}/.github"
-echo "$readme" > "${build_dir}/.github/README.md"
+if [ -z "$status" ]; then
+    readme="Build based on commit ${rev}.\n${timestamp}"
+else
+    readme="Build based on commit ${rev} with local changes.\n${timestamp}"
+fi
+mkdir -p "${outdir}/.github"
+echo -e "$readme" > "${outdir}/.github/README.md"
 
-# Fin
-echo "Done"
+_info "Build done"
